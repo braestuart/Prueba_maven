86425999a6652670b674497e6311d21be53ffd66
braestuart <braynernavarro@gmail.com>
2015-02-28 11:50:21 -0600

    Hola mi primera modificación

--- src/main/java/pruebagithub/App.java
+++ src/main/java/pruebagithub/App.java
@@ -9,5 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+	Sumar(12,3);
+	
+    }
+    public static void Sumar( int a, int b )
+    {
+	int c=a+b;
+        System.out.println( "La suma es "+c );
     }
 }
